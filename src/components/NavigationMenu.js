3c7772454176931b4c779735e31d0b42cdffc942
Elijah Dixon <dixonmail95@gmail.com>
2023-03-21 16:32:52 +0000

    nav bar style update

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -1,36 +1,43 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-
 import { Link } from "react-router-dom";
+
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import MenuBackground from '../img/menuimg.jpg'
+
+function handleClick(event) {
+    event.preventDefault();
+}
 
 export const NavigationBar = () =>  {
     
     
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                    </IconButton>
-                    <Typography sx={{ m: 2 }} variant="h6" color="inherit" component="div">
-                        <Link to="/">Home</Link>
-                    </Typography>
-                    <Typography sx={{ m: 2 }} variant="h6" color="inherit" component="div">
-                        <Link to="/playchess">Play</Link>
-                    </Typography>
-                    <Typography sx={{ m: 2 }} variant="h6" color="inherit" component="div">
-                        <Link to="/puzzles">Puzzles</Link>
-                    </Typography>
-                    <Typography sx={{ m: 2 }} variant="h6" color="inherit" component="div">
-                        <Link to="/users">Users</Link>
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <div style={{ backgroundImage: `url(${ MenuBackground })`, backgroundSize: "cover", backgroundPosition: "0% -65%" }} role="presentation" onClick={handleClick}>
+            <Breadcrumbs  aria-label="breadcrumb"
+            sx={{ 
+                border: 1,
+                p: 3,
+                boxShadow: 4,
+                borderRadius: '15px',
+                fontWeight: 'bold'
+            }}
+            >
+                <Link to="/">
+                <HomeOutlinedIcon />
+                </Link>
+                <Link style={{ textDecoration: 'none' }}  color="inherit" Link to="/playchess">
+                    Play
+                </Link>
+                <Link style={{ textDecoration: 'none' }} color="inherit" Link to="/puzzles">
+                    Puzzles
+                </Link>
+                <Link style={{ textDecoration: 'none' }} color="inherit" Link to="/users">
+                    Users
+                </Link>
+            </Breadcrumbs>
+        </div>
     );
 }
 
