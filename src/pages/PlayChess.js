e4a63f70e445c3d8594a704cb7185bed85d38ca5
Vladislav Kalinovskij <119266924+vk300115@users.noreply.github.com>
2023-04-05 23:34:40 +0100

    playChess user button added, styling added

--- src/pages/PlayChess.js
+++ src/pages/PlayChess.js
@@ -1,9 +1,48 @@
 import { ChessGame } from "../components/ChessGame";
+import { Container } from "@mui/material";
+import { CurrentUserCard } from "../components/SelectedUser";
+import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
+
 
 export const PlayChess = () => {
+  let getCurrentUser = JSON.parse(localStorage.getItem("currentUser")) || []
   return (
     <div>
       <ChessGame />
+      <Container style={{
+      display: "flex",
+      textAlign: "center",
+    }}>
+    {getCurrentUser.length === 0 ? (
+        <Container style={{
+          display: "flex",
+          margin: "30px auto",
+          justifyContent: "center"
+        }}>
+          
+          <Link style={{ textDecoration: 'none',color: '#463730'}} color="inherit" Link to="/users">
+            <Button type="submit" size="medium"  variant="contained"
+                style={{
+                background: "#839A9B", 
+                color: "black", 
+                marginBottom: "30px",
+                fontWeight:"bold"}}>
+                Please select user!
+                
+            </Button>
+          </Link>
+          
+        </Container>
+      ) : (
+        <Container style={{
+          display: "flex",
+          justifyContent: "center",
+        }}>
+        <CurrentUserCard />
+        </Container>
+      )}
+    </Container>
     </div>
   );
 };
